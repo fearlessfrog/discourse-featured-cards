288ac2f335e8b1096a14d8aee47d6917b2bc6d3e
fearlessfrog <fearlessfrog@zoomread.com>
2023-01-03 19:55:48 -0800

    Update featured-cards.js

--- javascripts/discourse/components/featured-cards.js
+++ javascripts/discourse/components/featured-cards.js
@@ -77,10 +77,11 @@ export default Component.extend({
     if (currentRoute) {
       if (settings.show_on === "homepage") {
         console.log('Route : ');
-        console.debug(currentRoute);
-        //console.log("Route params: ' + currentRoute.attributes.category.name);
+        console.debug(currentRoute);        
         console.log('RouteName: ' + currentRouteName);
-        return currentRoute.attributes.category.name == "Articles";
+        if (currentRoute.attributes.category) {
+          return currentRoute.attributes.category.name == "Articles";
+        }
         // return currentRouteName == `discovery.${defaultHomepage()}`;
       } else if (settings.show_on === "top_menu") {
         const topMenuRoutes = this.siteSettings.top_menu
